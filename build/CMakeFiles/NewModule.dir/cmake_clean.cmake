8a8109f08f6207f29a4ed159d5f90af5aca51b78
Payton Kennedy <pkennedy@odd11.cs.ohiou.edu>
2020-02-05 14:49:57 -0500

    Initial commmit

---
+++ build/CMakeFiles/NewModule.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewModule.dir/GLViewNewModule.cpp.o"
+  "CMakeFiles/NewModule.dir/NewModuleWayPoints.cpp.o"
+  "CMakeFiles/NewModule.dir/main.cpp.o"
+  "NewModule"
+  "NewModule.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewModule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
